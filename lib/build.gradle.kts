e010cd965dd4f5084e5b4b039405af940b2d4f43
Aliaksandr Arashkevich <aarashkevich@sws.by>
2023-07-31 22:36:19 +0300

    More links and comments

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   id("io.freefair.lombok") version "8.1.0"
 }
 
-group = "by.sws.core"
+group = "com.google"
 version = libs.versions.sdk.get()
 
 java {
@@ -14,6 +14,7 @@ java {
   withSourcesJar()
 }
 
+// https://docs.gradle.org/current/userguide/performance.html#run_the_compiler_as_a_separate_process
 tasks.withType<JavaCompile>().configureEach {
   options.isFork = true
 }
@@ -26,24 +27,29 @@ dependencies {
   // This dependency is used internally, and not exposed to consumers on their own compile classpath.
   implementation("com.google.guava:guava:31.1-jre")
 
+  // Example of consuming library bundle (set of dependencies that are going together)
   testImplementation(libs.bundles.junit)
 }
 
 tasks.withType<Test> {
   useJUnitPlatform()
+  // https://junit.org/junit5/docs/snapshot/user-guide/#writing-tests-parallel-execution
   systemProperty("junit.jupiter.extensions.autodetection.enabled", true)
   systemProperty("junit.jupiter.execution.parallel.enabled", true)
   systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
   systemProperty("junit.jupiter.execution.parallel.mode.classes.default", "concurrent")
 }
 
+// Set to 1 exactly if you have integration tests that are going to reuse testcontainers
+// Set to availableProcessors() / 2 for functional test for maximum performance
+// Read more https://docs.gradle.org/current/userguide/performance.html#execute_tests_in_parallel
 tasks.withType<Test>().configureEach {
   maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
 }
 
 publishing {
   publications {
-    create<MavenPublication>("swsLibrary") {
+    create<MavenPublication>("googleRepository") {
       groupId = project.group.toString()
       artifactId = rootProject.name
       version = project.version.toString()
